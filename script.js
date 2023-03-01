f00f70ca1d978b63030a02b4fe355cc12cdd86a4
mr-selviraja <i.am.selviraja.p@gmail.com>
2023-03-01 15:08:43 +0530

    Project completed

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ btnToggle.addEventListener("click", () => {
   btnToggle.classList.toggle("toggle-btn-active");
   navLinks.classList.toggle("navlinks-active");
 });
+
+alert(
+  "Thank you so much for the Opportunity! Although This webiste can be develped a bit better. I've kept my efforts to complete the project with severe Fever.\n\nLooking forward for much challenging websites to build as part of this Internship.\n\nThank you so much Sir!!!"
+);
